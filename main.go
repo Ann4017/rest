ee3a881267bc75583a1e035cbd2a6f3a3e9d74ba
Ann4017 <121239306+Ann4017@users.noreply.github.com>
2023-05-12 11:12:08 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"rest/db"
 
@@ -10,32 +11,53 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	db := &db.C_db{}
-	h := &handlers.C_user_handler{
-		C_db: db,
+type Server struct {
+	PC_db *db.C_db
+	PC_h  *handlers.C_user_handler
+}
+
+func (s *Server) Init(_s_ini_path string) error {
+	s.PC_db = &db.C_db{}
+	s.PC_h = &handlers.C_user_handler{
+		C_db: s.PC_db,
 	}
 
-	err := db.Load_config("config/config.ini")
+	err := s.PC_db.Load_config(_s_ini_path)
 	if err != nil {
 		fmt.Println("Load_config err:", err)
+		return err
 	}
 
-	err = db.Connect_db()
+	err = s.PC_db.Connect_db()
 	if err != nil {
 		fmt.Println("Connect_db err:", err)
+		return err
 	}
 
+	return nil
+}
+
+func (s *Server) Run() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "main")
 	})
-	r.HandleFunc("/users", h.Select_users)
-	r.HandleFunc("/user/{id}", h.Select_user).Methods("GET")
-	r.HandleFunc("/user", h.Insert_user).Methods("POST")
-	r.HandleFunc("/user/{id}", h.Update_user).Methods("PUT")
-	r.HandleFunc("/user/{id}", h.Delete_user).Methods("DELETE")
+	r.HandleFunc("/users", s.PC_h.Select_users)
+	r.HandleFunc("/user/{id}", s.PC_h.Select_user).Methods("GET")
+	r.HandleFunc("/user", s.PC_h.Insert_user).Methods("POST")
+	r.HandleFunc("/user/{id}", s.PC_h.Update_user).Methods("PUT")
+	r.HandleFunc("/user/{id}", s.PC_h.Delete_user).Methods("DELETE")
 
 	http.ListenAndServe(":8000", r)
+}
+
+func main() {
+	s := Server{}
+	err := s.Init("config/config.ini")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Run()
 }
